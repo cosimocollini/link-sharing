d72b1956dd465ac5f63def724ed14f4196ad6d03
Cosimo Collini <cosicollini@gmail.com>
2025-04-22 19:33:22 +0200

    update config and packages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,22 @@
+import Components from 'unplugin-vue-components/vite';
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import svgLoader from 'vite-svg-loader';
+import RekaResolver from 'reka-ui/resolver';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()],
+  plugins: [
+    vue(),
+    svgLoader(),
+    Components({
+      dirs: ['src/components'],
+      extensions: ['vue'],
+      dts: true,
+      resolvers: [RekaResolver()]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
